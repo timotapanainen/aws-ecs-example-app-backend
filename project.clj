b497336afeb0ef22cc88e82054b5672cce61ca44
timotapanainen@users.noreply.github.com <timo.tapanainen@gmail.com>
2018-01-31 00:20:52 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject aws-ecs-example-app-backend "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.pedestal/pedestal.service "0.5.3"]
+                 [io.pedestal/pedestal.route "0.5.3"]
+                 [io.pedestal/pedestal.jetty "0.5.3"]
+                 [org.slf4j/slf4j-simple "1.7.21"]]
+  :main myapp-backend.core
+  :profiles {
+             :uberjar {
+                       :aot  :all
+                       :main myapp-backend.core}})
---
+++ src/myapp_backend/core.clj
@@ -0,0 +1,28 @@
+(ns myapp-backend.core
+    (:require
+      [io.pedestal.http :as http]
+      [io.pedestal.http.route :as route])
+    (:gen-class))
+
+
+(defn respond-health [request]
+      {:status 200 :body "Ok"})
+
+(defn respond-hello [request]
+      {:status  200
+       :headers {"Content-Type" "text/html"}
+       :body    "Backend responding"})
+
+(def routes
+  (route/expand-routes
+    #{["/api/health" :get respond-health :route-name :health]
+      ["/api/" :get respond-hello :route-name :hello]}))
+
+(defn create-server []
+      (http/create-server
+        {::http/routes routes
+         ::http/type   :jetty
+         ::http/port   8080}))
+
+(defn -main [& args]
+      (http/start (create-server)))
