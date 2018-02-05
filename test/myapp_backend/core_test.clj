6689696cac1f6aac5243178ea1bcc1e4813af51f
timotapanainen@users.noreply.github.com <timo.tapanainen@gmail.com>
2018-02-05 14:35:44 +0200

    remove test for now

--- test/myapp_backend/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns myapp-backend.core-test
-  (:require [clojure.test :refer :all]
-            [myapp-backend.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
